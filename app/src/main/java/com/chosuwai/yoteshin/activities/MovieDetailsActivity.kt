120b1725195a702cef7ce338725d981e36240c69
Cho Su Wai <chosuwai@gmail.com>
2023-01-13 23:37:39 +0630

    Movie Details Screen POC

---
+++ app/src/main/java/com/chosuwai/yoteshin/activities/MovieDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.chosuwai.yoteshin.activities
+
+import android.os.Bundle
+import com.chosuwai.yoteshin.R
+
+class MovieDetailsActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
